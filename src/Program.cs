a19a04d32fb985846fc92937b67f8a22581063c7
Diego <alvarodiego@gmail.com>
2021-01-27 11:08:08 -0300

    Leitura do arquivo e dictionary prontos

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace src
 {
@@ -6,7 +8,50 @@ namespace src
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("====  Seção 15: Exercício proposto dictionary  ====");
+
+
+            string caminhPastaIo = @"C:\Users\alvar\OneDrive\Workspaces\ws-vs2019\secao-15-exercicio-proposto-dictionary\io\";
+            string arquivoInput = @"input.txt";
+
+            FileStream fsInput = null;
+            try
+            {
+                fsInput = new FileStream(caminhPastaIo + arquivoInput, FileMode.Open);
+
+                Dictionary<string, int> candidatos = new Dictionary<string, int>();
+
+                using (StreamReader rsInput = new StreamReader(fsInput))
+                {
+                    while (!rsInput.EndOfStream)
+                    {
+                        string[] linha = rsInput.ReadLine().Split(',');
+                        if (!candidatos.ContainsKey(linha[0]))
+                        {
+                            candidatos[linha[0]] = int.Parse(linha[1]);
+                        }
+                        else
+                        {
+                           int temp = candidatos[linha[0]];
+                           candidatos[linha[0]] = temp + int.Parse(linha[1]);
+                        }
+                        
+                    }
+                }
+
+                foreach (KeyValuePair<string, int> candidato in candidatos)
+                {
+                    Console.WriteLine(candidato.Key +": " + candidato.Value);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+            finally
+            {
+                if (fsInput != null) fsInput.Close();
+            }
         }
     }
 }
